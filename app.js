97d1c3c7a778f9501bad5980a2f72f6a766ab2f2
Techkny <bobadeprojects@gmail.com>
2019-04-10 16:40:34 +0100

    Created First Test to check for addition of two numbers

---
+++ app.js
@@ -0,0 +1,4 @@
+function addTwoNumbers(num1, num2) {
+  //... May the Code Be With You In Jesus Name
+}
+exports.addTwoNumbers = addTwoNumbers;
---
+++ app.test.js
@@ -0,0 +1,2 @@
+const { addTwoNumbers } = require("./app");
+test("Add two numbers 3 and 7", expect(addTwoNumbers(3, 7)).toBe(10));
