1c8635faa2fc3cc9e3250c2a1f73de4dc2c0b9ff
Techkny <bobadeprojects@gmail.com>
2019-04-10 18:33:05 +0100

    Renamed app.js to difference.js and the test file

--- difference/difference.test.js
+++ difference/difference.test.js
@@ -1,4 +1,4 @@
-const differenceBetweenTwoNumbers = require("./app");
+const differenceBetweenTwoNumbers = require("./difference");
 
 test("Difference Between 1963 and 1295", () => {
   expect(differenceBetweenTwoNumbers(1963, 1295)).toBe(668);
--- division/division.js
+++ division/division.js
@@ -1,6 +1,5 @@
 function secondDividesFirst(num1, num2) {
   //... May the Code Be With You In Jesus Name
-  return num1 / num2;
 }
 
 module.exports = secondDividesFirst;
