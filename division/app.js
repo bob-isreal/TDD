c5091235b8b3032af08dc4887dfd45366f89158f
Techkny <bobadeprojects@gmail.com>
2019-04-10 18:03:00 +0100

    Test for division of two numbers

---
+++ division/app.js
@@ -0,0 +1,6 @@
+function secondDividesFirst(num1, num2) {
+  //... May the Code Be With You In Jesus Name
+  return num1 / num2;
+}
+
+module.exports = secondDividesFirst;
---
+++ division/app.test.js
@@ -0,0 +1,31 @@
+const secondDividesFirst = require("./app");
+
+test("Divide 100 by 10", () => {
+  let num1 = 100,
+    num2 = 10;
+  expect(secondDividesFirst(num1, num2)).toBe(10);
+});
+
+test("Divide 56 by 26 to give 2 Decimal Place", () => {
+  let num1 = 56,
+    num2 = 26;
+  expect(secondDividesFirst(num1, num2)).toEqual(2.15);
+});
+
+test("Divide a Number by a String", () => {
+  let num1 = 56,
+    num2 = "random string";
+  expect(secondDividesFirst(num1, num2)).toBeNaN();
+});
+
+test("Divide a Number by a String", () => {
+  let num1 = 789,
+    num2 = "random string";
+  expect(secondDividesFirst(num1, num2)).toBe(NaN);
+});
+
+test("Divide an Even Number by 2 (should not give float number)", () => {
+  let num1 = 788,
+    num2 = 2;
+  expect(secondDividesFirst(num1, num2)).toBe(num1 % num2);
+});
