57a11e02668fb54f6b0195c7bd04864d9bbec006
Techkny <bobadeprojects@gmail.com>
2019-04-10 17:41:00 +0100

    Finish test for multiplication of indefinite amount of arguments

---
+++ indefiniteMultiply/app.js
@@ -0,0 +1,5 @@
+function multiplyIndefiniteNumbers(num1, num2) {
+  //... May the Code Be With You In Jesus Name
+}
+
+module.exports = multiplyIndefiniteNumbers;
---
+++ indefiniteMultiply/app.test.js
@@ -0,0 +1,35 @@
+const multiplyIndefiniteNumbers = require("./app");
+
+test("Multiply five numbers 2,3,4,5,5", () => {
+  expect(multiplyIndefiniteNumbers(2, 3, 4, 5, 5)).toBe(600);
+});
+
+test("Multply six numbers 1,2,3,4,5,6", () => {
+  expect(multiplyIndefiniteNumbers(1, 2, 3, 4, 5, 6)).toBe(720);
+});
+
+test("Multiply an array of numbers of unknown length starting from 0", () => {
+  let arr = [];
+  for (let i = 0; i < 39; i++) {
+    arr.push(i);
+  }
+  let ans = 0;
+  arr.forEach(value => {
+    ans *= value;
+  });
+
+  expect(multiplyIndefiniteNumbers(arr)).toBe(ans);
+});
+
+test("Multiply an array of numbers of unknown length excluding 0", () => {
+  let arr = [];
+  for (let i = 1; i < 16; i++) {
+    arr.push(i);
+  }
+  let ans = 1;
+  arr.forEach(value => {
+    ans *= value;
+  });
+
+  expect(multiplyIndefiniteNumbers(arr)).toBe(ans);
+});
